f0d8494033bb093ea4dfa7b11e051350cc206278
智衡 高 <zhihenggao@zhihengdeMacBook-Pro.local>
2018-06-08 15:38:25 +0800

    change all to green

--- MapDemo.js
+++ MapDemo.js
@@ -8,9 +8,9 @@
 		new BMap.Point(116.399, 39.910),
 		new BMap.Point(116.405, 39.920),
 		new BMap.Point(116.425, 39.900)
-	], {strokeColor:"blue", strokeWeight:2, strokeOpacity:0.5});   //创建折线
+	], {strokeColor:"green", strokeWeight:2, strokeOpacity:0.5});   //创建折线
 	
-	var circle = new BMap.Circle(point,500,{strokeColor:"blue", strokeWeight:2, strokeOpacity:0.5}); //创建圆
+	var circle = new BMap.Circle(point,500,{strokeColor:"green", strokeWeight:2, strokeOpacity:0.5}); //创建圆
 	
 	var polygon = new BMap.Polygon([
 		new BMap.Point(116.387112,39.920977),
@@ -18,7 +18,7 @@
 		new BMap.Point(116.394226,39.917988),
 		new BMap.Point(116.401772,39.921364),
 		new BMap.Point(116.41248,39.927893)
-	], {strokeColor:"blue", strokeWeight:2, strokeOpacity:0.5});  //创建多边形
+	], {strokeColor:"green", strokeWeight:2, strokeOpacity:0.5});  //创建多边形
 	
 	var pStart = new BMap.Point(116.392214,39.918985);
 	var pEnd = new BMap.Point(116.41478,39.911901);
@@ -27,7 +27,7 @@
 		new BMap.Point(pEnd.lng,pStart.lat),
 		new BMap.Point(pEnd.lng,pEnd.lat),
 		new BMap.Point(pStart.lng,pEnd.lat)
-	], {strokeColor:"red", strokeWeight:2, strokeOpacity:0.5});  //创建矩形
+	], {strokeColor:"green", strokeWeight:2, strokeOpacity:0.5});  //创建矩形
 	
 	//添加覆盖物
 	function add_overlay(){
